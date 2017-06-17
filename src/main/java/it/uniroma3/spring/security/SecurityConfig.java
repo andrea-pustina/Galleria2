00b1cdbc7c5d5c47092b4361f740e92e5d99b6ed
Unknown <apustin@gmail.com>
2017-06-17 13:34:46 +0200

    management.css managementforms.css, navbar diversa se loggato

--- src/main/java/it/uniroma3/spring/security/SecurityConfig.java
+++ src/main/java/it/uniroma3/spring/security/SecurityConfig.java
@@ -49,6 +49,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth
             .inMemoryAuthentication()
-                .withUser("user").password("pass").roles("USER");
+                .withUser("user").password("pass").roles("ADMIN");
     }
 }
